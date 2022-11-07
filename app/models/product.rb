9198ac79962c80b37f6787aff8efb80b92de2ee1
nanhsirkalapoG <togopalakrishnan@gmail.com>
2022-11-07 09:24:35 +0530

    Adds annotate gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :string(255)
+#  price       :decimal(10, 2)
+#  title       :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  external_id :string(255)
+#  user_id     :integer
+#
 class Product < ApplicationRecord
   belongs_to :user
   has_many :string_custom_fields, as: :customizable, class_name: 'StringCustomField', :dependent => :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string(255)
+#  first_name :string(255)
+#  last_name  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+  has_many :products
 end
