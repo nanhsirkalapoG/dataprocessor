44da693cbe93fdb90a9fa6e5897dfa424c5e8b2b
nanhsirkalapoG <togopalakrishnan@gmail.com>
2022-11-08 20:02:39 +0530

    Improves the custom field processing logic

--- app/services/custom_field_service.rb
+++ app/services/custom_field_service.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class CustomFieldService
-  FIELD_SIZE_LIMIT = {
-    date: 10,
-    number: 10,
-    string: 10
-  }.freeze
-  VALID_FIELD_TYPES = %w[date number string].freeze
+  attr_reader :entity, :custom_fields
 
   def initialize(entity, custom_fields)
     @entity = entity
@@ -16,10 +11,8 @@ class CustomFieldService
   def process_custom_fields
     grouped_custom_fields = group_custom_fields_by_data_type
 
-    grouped_custom_fields.each do |type, custom_fields|
-      filtered_custom_fields = filtered_custom_fields(custom_fields, type)
-
-      process_custom_fields_by_type(filtered_custom_fields, type)
+    grouped_custom_fields.each do |type, custom_fields_by_type|
+      process_custom_fields_by_type(custom_fields_by_type, type)
     end
   end
 
@@ -27,40 +20,29 @@ class CustomFieldService
 
   def group_custom_fields_by_data_type
     custom_fields_by_type = {}
-    VALID_FIELD_TYPES.each do |type|
-      custom_fields_by_type[type] = @custom_fields.select { |field| field['data_type'] == type }
+    CustomField::VALID_CUSTOM_FIELD_TYPES.each do |type|
+      custom_fields_by_type[type] = custom_fields.select { |field| field['data_type'] == type }
     end
     custom_fields_by_type
   end
 
-  # Limit the number of records on each type
-  # Should move the validation at model level
-  def filtered_custom_fields(custom_fields, type)
-    size_limit = FIELD_SIZE_LIMIT[type.to_sym]
-    custom_fields.first(size_limit)
-  end
-
   def process_custom_fields_by_type(custom_fields, data_type)
-    existing_custom_fields_id = @entity.custom_fields.by_data_type(data_type).pluck(:id)
-    new_custom_fields_id = []
+    cleanup_custom_fields(custom_fields, data_type)
 
     custom_fields.each do |custom_field|
-      field = CustomField.find_or_initialize_by(customizable: @entity,
+      field = CustomField.find_or_initialize_by(customizable: entity,
                                                 field_name: custom_field[:field_name],
                                                 data_type: data_type)
       field.value = custom_field[:value]
-      field.save!
-      new_custom_fields_id << field.id
-    rescue StandardError => e
-      Rails.logger.error("Could not process the custome field: #{custom_field}, error: #{e.message}")
-    end
+      break unless field.valid?
 
-    cleanup_custom_fields(existing_custom_fields_id, new_custom_fields_id)
+      entity.custom_fields << field
+    end
   end
 
-  def cleanup_custom_fields(existing_custom_fields_id, new_custom_fields_id)
-    ids_to_delete = existing_custom_fields_id - new_custom_fields_id
+  def cleanup_custom_fields(custom_fields, data_type)
+    custom_field_names = custom_fields.map { |field| field[:field_name] }
 
-    CustomField.where(id: ids_to_delete).destroy_all
+    entity.custom_fields.by_data_type(data_type).where.not(field_name: custom_field_names).destroy_all
   end
 end
