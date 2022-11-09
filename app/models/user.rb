06354683fcc07a924d861c76789293f7f7a8a3f8
nanhsirkalapoG <togopalakrishnan@gmail.com>
2022-11-09 20:27:03 +0530

    Adds password_digest to users schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string(255)
-#  first_name :string(255)
-#  last_name  :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  email           :string(255)
+#  first_name      :string(255)
+#  last_name       :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
+
   validates :first_name, presence: true, null: false
   validates :last_name, presence: true, null: false
   validates :email, presence: true, uniqueness: true, null: false
