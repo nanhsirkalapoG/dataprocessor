14c7f337a44d1f0a1e6c826149ce6f704b9d0905
nanhsirkalapoG <togopalakrishnan@gmail.com>
2022-11-09 20:27:32 +0530

    Authenticates API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :authorized, except: %i[login register]
+
+  def current_user
+    @user = User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate
+    @user = User.find_by(email: params[:username])
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      return true
+    end
+    false
+  end
+
+  def authorized
+    render json: { message: 'Please login to continue!' }, status: :unauthorized and return unless logged_in?
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe '#index' do
-    before do
-      2.times { |_| FactoryBot.create(:user) }
-    end
+  let(:user) { FactoryBot.create(:user, first_name: 'test_user') }
 
-    it 'return all the users' do
-      get(:index)
-
-      expect(@response.code).to eq('200')
-      users = JSON.parse(@response.body)['users']
-      expect(users.count).to eq(2)
-    end
+  before do
+    session[:user_id] = user.id
   end
 
   describe '#show' do
-    let(:user) { FactoryBot.create(:user, first_name: 'test_user') }
-
-    context 'when the user is not present' do
-      it 'return resource not found error' do
-        get(:show, params: { id: -1 })
-
-        expect(@response.code).to eq('404')
-      end
-    end
-
     context 'when the user is present' do
       it 'return the user details' do
         get(:show, params: { id: user.id })
@@ -49,7 +31,7 @@ RSpec.describe UsersController, type: :controller do
 
     context 'when the input is valid' do
       it 'create the user' do
-        post(:create, params: { first_name: 'test', last_name: 'test', email: 'email@test.com' })
+        post(:create, params: { first_name: 'test', last_name: 'test', email: 'email@test.com', password: 'test' })
 
         expect(@response.code).to eq('200')
         expect(User.last.email).to eq('email@test.com')
@@ -58,8 +40,6 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe '#update' do
-    let(:user) { FactoryBot.create(:user, first_name: 'test_user') }
-
     context 'when the input is not valid' do
       it 'raise an error' do
         post(:update, params: { id: user.id, first_name: nil })
